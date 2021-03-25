ac057f1b982a313bf06322e4895d91b05895aa2c
Thiago Pedroso <78765933+Thi-jacare@users.noreply.github.com>
2021-03-25 17:58:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DIO.Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+
+            
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
